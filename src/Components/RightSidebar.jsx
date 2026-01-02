b9321462774639f1e1e8966eefbb52fe303f6ce4
Yogesh kumawat <yk573614@gmail.com>
2026-01-03 00:50:23 +0530

    Dashboard half completed

--- src/Components/RightSidebar.jsx
+++ src/Components/RightSidebar.jsx
@@ -6,7 +6,7 @@ import { FaFilePen } from "react-icons/fa6";
 
 const RightSidebar = () => {
   return (
-    <div className="h-178 bg-[#F3F2F2] w-60 flex flex-col gap-5 p-4">
+    <div className="h-178 bg-[#F3F2F2] w-70 flex flex-col gap-5 p-4">
       <div className="flex gap-4 p-2">
         <div className="h-14 w-14 flex justify-center items-center text-xl rounded-xl text-[#0C8208] bg-[#C6EFC3] font-bold">
           UN
@@ -20,33 +20,33 @@ const RightSidebar = () => {
         <p className="mb-3 text-[#0A2478] text-sm font-semibold">
           Streak : 2 day
         </p>
-        <div className="flex justify-between ">
+        <div className="flex justify-between px-2">
           <div className="flex flex-col items-center gap-1">
-            <div className="h-7 w-7 rounded-sm bg-[#FFF5CC] text-[#E29537] text-xl flex justify-center items-center">
+            <div className="h-8 w-8 rounded-sm bg-[#FFF5CC] text-[#E29537] text-xl flex justify-center items-center">
               <MdOutlineStar />
             </div>
             <p className="text-[0.6em]">Day 1</p>
           </div>
           <div className="flex flex-col items-center gap-1">
-            <div className="h-7 w-7 rounded-sm bg-[#FFF5CC] text-[#E29537] text-xl flex justify-center items-center">
+            <div className="h-8 w-8 rounded-sm bg-[#FFF5CC] text-[#E29537] text-xl flex justify-center items-center">
               <MdOutlineStar />
             </div>
             <p className="text-[0.6em]">Day 2</p>
           </div>
           <div className="flex flex-col items-center gap-1 text-[#ABABAB]">
-            <div className="h-7 w-7 rounded-sm text-xl flex justify-center items-center">
+            <div className="h-8 w-8 rounded-sm text-xl flex justify-center items-center">
               <MdOutlineStar />
             </div>
             <p className="text-[0.6em]">Day 3</p>
           </div>
           <div className="flex flex-col items-center gap-1 text-[#ABABAB]">
-            <div className="h-7 w-7 rounded-sm text-xl flex justify-center items-center">
+            <div className="h-8 w-8 rounded-sm text-xl flex justify-center items-center">
               <MdOutlineStar />
             </div>
             <p className="text-[0.6em]">Day 4</p>
           </div>
           <div className="flex flex-col items-center gap-1 text-[#ABABAB]">
-            <div className="h-7 w-7 rounded-sm text-xl flex justify-center items-center">
+            <div className="h-8 w-8 rounded-sm text-xl flex justify-center items-center">
               <MdOutlineStar />
             </div>
             <p className="text-[0.6em]">Day 5</p>
@@ -59,21 +59,21 @@ const RightSidebar = () => {
         </p>
         <div className="flex justify-evenly">
           <div className="flex flex-col items-center">
-            <div className="h-8 w-8 bg-[#EDEDED] rounded-sm flex justify-center items-center">
+            <div className="h-9 w-9 bg-[#EDEDED] rounded-sm flex justify-center items-center">
               <FiBookOpen />
             </div>
             <h6>128</h6>
             <p className="text-[0.7em]">Lessons</p>
           </div>
           <div className="flex flex-col items-center">
-            <div className="h-8 w-8 bg-[#EDEDED] rounded-sm flex justify-center items-center">
+            <div className="h-9 w-9 bg-[#EDEDED] rounded-sm flex justify-center items-center">
               <FaFlag />
             </div>
             <h6>272</h6>
             <p className="text-[0.7em]">Exercises</p>
           </div>
           <div className="flex flex-col items-center">
-            <div className="h-8 w-8 bg-[#EDEDED] rounded-sm flex justify-center items-center">
+            <div className="h-9 w-9 bg-[#EDEDED] rounded-sm flex justify-center items-center">
               <FaFilePen />
             </div>
             <h6>1</h6>
@@ -90,8 +90,7 @@ const RightSidebar = () => {
               <p className="text-[#0A2478] font-semibold">90%</p>
             </div>
             <div className="w-full h-1 rounded-full bg-[#D9D9D9]">
-              <div className="w-[90%] h-full bg-[#1BA53B] rounded-full">
-              </div>
+              <div className="w-[90%] h-full bg-[#1BA53B] rounded-full"></div>
             </div>
           </div>
           <div className="px-2 flex flex-col gap-1">
@@ -100,8 +99,7 @@ const RightSidebar = () => {
               <p className="text-[#0A2478] font-semibold">85%</p>
             </div>
             <div className="w-full h-1 rounded-full bg-[#D9D9D9]">
-              <div className="w-[85%] h-full bg-[#1BA53B] rounded-full">
-              </div>
+              <div className="w-[85%] h-full bg-[#1BA53B] rounded-full"></div>
             </div>
           </div>
           <div className="px-2 flex flex-col gap-1">
@@ -110,8 +108,7 @@ const RightSidebar = () => {
               <p className="text-[#0A2478] font-semibold">70%</p>
             </div>
             <div className="w-full h-1 rounded-full bg-[#D9D9D9]">
-              <div className="w-[70%] h-full bg-[#1BA53B] rounded-full">
-              </div>
+              <div className="w-[70%] h-full bg-[#1BA53B] rounded-full"></div>
             </div>
           </div>
           <div className="px-2 flex flex-col gap-1">
@@ -120,8 +117,7 @@ const RightSidebar = () => {
               <p className="text-[#0A2478] font-semibold">100%</p>
             </div>
             <div className="w-full h-1 rounded-full bg-[#D9D9D9]">
-              <div className="h-full bg-[#1BA53B] rounded-full">
-              </div>
+              <div className="h-full bg-[#1BA53B] rounded-full"></div>
             </div>
           </div>
         </div>
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -10,50 +10,112 @@ const Dashboard = () => {
       <div className="flex justify-between">
         <Sidebar />
         <div>
-          <div className="flex gap-4">
+          <div className="flex gap-8">
             <div className="flex flex-col gap-4">
-              <div className="h-37 w-85 px-6 py-3 flex flex-col justify-between rounded-lg bg-[#F3F2F2]">
+              <div className="h-40 w-100 px-6 py-3 flex flex-col justify-between rounded-lg bg-[#F3F2F2]">
                 <p className="text-[#606060]">Continue Learning</p>
                 <div>
                   <h3 className="text-xl font-bold">UI / UX</h3>
                   <p className="text-[#606060]">Chapter - 16</p>
                 </div>
                 <div className="flex items-center gap-4">
-                  <button className="bg-[#0DC386] px-5 py-1 text-sm rounded-lg text-white font-semibold">Continue</button>
-                  <p className="text-[#6F6F6F] font-bold text-sm">Next : Chapter - 17</p>
+                  <button className="bg-[#0DC386] hover:bg-[#0fdc97] px-5 py-1 text-sm rounded-lg text-white font-semibold">
+                    Continue
+                  </button>
+                  <p className="text-[#6F6F6F] font-bold text-sm">
+                    Next : Chapter - 17
+                  </p>
                 </div>
               </div>
-              <div className="h-37 w-85 px-6 py-3 flex flex-col justify-between rounded-lg bg-[#F3F2F2]">
+              <div className="h-45 w-100 px-6 py-3 flex flex-col justify-between rounded-lg bg-[#F3F2F2]">
                 <p className="text-[#606060]">Upcoming Deadlines</p>
                 <div className="flex flex-col gap-2">
                   <div className="flex gap-2">
                     <div className="h-6 w-6 rounded-full border-3 border-[#676767] bg-white flex justify-center items-center">
                       <div className="h-4 w-1 rounded-full bg-[#D00000]"></div>
                     </div>
-                    <p className="font-semibold">Project - 1 <span className="text-[#606060] font-normal text-sm">Due Today</span> </p>
+                    <p className="font-semibold">
+                      Project - 1{" "}
+                      <span className="text-[#606060] font-normal text-sm">
+                        Due Today
+                      </span>{" "}
+                    </p>
                   </div>
                   <div className="flex gap-2">
                     <div className="h-6 w-6 rounded-full border-3 border-[#676767] bg-white flex justify-center items-center">
                       <div className="h-[0.8em] w-[0.8em] rounded-full bg-[#178431]"></div>
                     </div>
-                    <p className="font-semibold">Project - 2 <span className="text-[#606060] font-normal text-sm">Due May 23</span> </p>
+                    <p className="font-semibold">
+                      Project - 2{" "}
+                      <span className="text-[#606060] font-normal text-sm">
+                        Due May 23
+                      </span>{" "}
+                    </p>
                   </div>
                   <div className="flex gap-2">
                     <div className="h-6 w-6 rounded-full border-3 border-[#676767] bg-white flex justify-center items-center">
                       <div className="h-[0.8em] w-[0.8em] rounded-full bg-[#178431]"></div>
                     </div>
-                    <p className="font-semibold">Project - 3 <span className="text-[#606060] font-normal text-sm">Due June 12</span> </p>
+                    <p className="font-semibold">
+                      Project - 3{" "}
+                      <span className="text-[#606060] font-normal text-sm">
+                        Due June 12
+                      </span>{" "}
+                    </p>
+                  </div>
+                </div>
+                <button className="text-[#D0793E] text-xs font-semibold self-end cursor-pointer">View All</button>
+              </div>
+            </div>
+            <div className="h-89 w-100 px-6 py-3 flex flex-col justify-between rounded-lg bg-[#F3F2F2]">
+              <p className="text-[#606060]">Recent Activity</p>
+              <div className="bg-white p-4 rounded-lg h-65 flex flex-col justify-between">
+                <div className="flex flex-col gap-1">
+                  <div className="flex justify-between">
+                    <p className="font-semibold">Color Combinations</p>
+                    <p className="text-[#606060]">75%</p>
+                  </div>
+                  <div className="w-full h-1 rounded-full bg-[#D9D9D9]">
+                    <div className="w-[75%] h-full bg-[#875ABD] rounded-full"></div>
+                  </div>
+                </div>
+                <div className="border border-[#D9D9D9]"></div>
+                <div className="flex flex-col gap-1">
+                  <div className="flex justify-between">
+                    <p className="font-semibold">Design Principals</p>
+                    <p className="text-[#606060]">53%</p>
+                  </div>
+                  <div className="w-full h-1 rounded-full bg-[#D9D9D9]">
+                    <div className="w-[53%] h-full bg-[#875ABD] rounded-full"></div>
+                  </div>
+                </div>
+                <div className="border border-[#D9D9D9]"></div>
+                <div className="flex flex-col gap-1">
+                  <div className="flex justify-between">
+                    <p className="font-semibold">Introduction</p>
+                    <p className="text-[#606060]">100%</p>
+                  </div>
+                  <div className="w-full h-1 rounded-full bg-[#D9D9D9]">
+                    <div className="h-full bg-[#875ABD] rounded-full"></div>
+                  </div>
+                </div>
+                <div className="border border-[#D9D9D9]"></div>
+                <div className="flex flex-col gap-1">
+                  <div className="flex justify-between">
+                    <p className="font-semibold">Introduction</p>
+                    <p className="text-[#606060]">85%</p>
+                  </div>
+                  <div className="w-full h-1 rounded-full bg-[#D9D9D9]">
+                    <div className="w-[85%] h-full bg-[#875ABD] rounded-full"></div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="h-78 w-90 px-6 py-3 flex flex-col justify-between rounded-lg bg-[#F3F2F2]">
-              <p className="text-[#606060]">Recent Activity</p>
+              <button className="text-[#D0793E] text-xs font-semibold self-end cursor-pointer">View All</button>
             </div>
           </div>
           <div></div>
         </div>
-        <RightSidebar/>
+        <RightSidebar />
       </div>
     </div>
   );
